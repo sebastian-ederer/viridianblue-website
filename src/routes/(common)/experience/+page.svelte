eca90ae433135c6b735b453bc2f8f489ca91c1bf
sebastian-ederer <sebi.ederer@gmx.de>
2025-03-01 02:05:45 +0100

    added coming soon text for experience page

--- src/routes/(common)/experience/+page.svelte
+++ src/routes/(common)/experience/+page.svelte
@@ -1,1 +1,13 @@
-<h1>Experience</h1>
+<div class="experience">
+	<h1>Experience</h1>
+	<p>Coming soon</p>
+</div>
+
+<style>
+	.experience {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
